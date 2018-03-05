fedac8d99eb46d87275abb652267581f688bb129
Lamp <lamp@vinova.sg>
2018-03-05 16:22:03 +0700

    Correct the mainTabbar

--- Prelens-jinny/Prelens-jinny/PRTabbarMainViewController.swift
+++ Prelens-jinny/Prelens-jinny/PRTabbarMainViewController.swift
@@ -9,22 +9,55 @@
 import UIKit
 import RxSwift
 
+protocol TabbarMainViewControllerDelegate: class {
+    func btnDrawerTabbarTapped()
+    func tabbarSelected(index: Int)
+}
+
 class PRTabbarMainViewController: UITabBarController {
     
-    //MARK: Declare 3 viewcontroller in Tabbar
+  //  weak var delagateBtnDrawer: TabbarMainViewControllerDelegate?
     
+    //MARK: Declare 3 viewcontroller in Tabbar
+    var vcProfile: UIViewController!
+    var vcVocher: UIViewController!
+    var vcNews: UIViewController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        delegate = self
+      //  delegate = self as! UITabBarControllerDelegate
     
         setUpTabbar()
         
     }
     
     func setUpTabbar() {
+        //we setup the delegate, the photo for the tapped or untapped of the tabbar item
+        //...
+        vcVocher = UIViewController()
+        vcVocher.tabBarItem = setBarItem(selectedImage: nil, normalImage: nil)
         
+        vcProfile = UIViewController()
+        vcProfile.tabBarItem = setBarItem(selectedImage: nil, normalImage: nil)
+        
+        vcNews = UIViewController()
+        vcNews.tabBarItem = setBarItem(selectedImage: nil, normalImage: nil)
+        //...
+        let tabbarListVC: [UIViewController] = [vcProfile, vcVocher, vcNews]
+        
+        //IphoneX configuration
+//        for controller in tabbarListVC {
+//            if DeviceTypeHelper.getDeviceType() != NCSDeviceType.small {
+//                if Device() == .iPhoneX || Device() == .simulator(.iPhoneX) {
+//                    controller.tabBarItem.imageInsets = tabIconInsetsX
+//                } else {
+//                    controller.tabBarItem.imageInsets = tabIconInsets
+//                }
+//            }
+//        }
+        
+        addViewControllerToTabbar(listViewController: tabbarListVC)
     }
     
     //Config item when it was tapped on
@@ -32,4 +65,57 @@ class PRTabbarMainViewController: UITabBarController {
         let item = UITabBarItem(title: nil, image: normalImage, selectedImage: selectedImage)
         return item
     }
+    
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+   //     delagateBtnDrawer?.tabbarSelected(index: tabBarController.selectedIndex)
+    //  tabLeft.setIndexSelected(index: tabBarController.selectedIndex)
+        
+        //MARK: Delegate Action here when item was tapped
+    }
+    
+    //Adding the tabbar Viewcontroller into list
+    func addViewControllerToTabbar(listViewController: [UIViewController]) {
+        var listNavigationController = [UINavigationController]()
+        for vc in listViewController {
+            let nc = UINavigationController(rootViewController: vc)
+            listNavigationController.append(nc)
+        }
+        //---
+        self.viewControllers = listNavigationController
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
