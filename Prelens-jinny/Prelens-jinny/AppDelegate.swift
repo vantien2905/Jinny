757671ef128cc36f154addb9d103fec61afb74e4
Rio <rio@vinova.sg>
2018-03-08 14:58:38 +0700

    Design UI: Add Membership

--- Prelens-jinny/Prelens-jinny/AppDelegate.swift
+++ Prelens-jinny/Prelens-jinny/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     func goToLogin() {
-        let vc  = UINavigationController(rootViewController:RedeemViewController())
+        let vc  = UINavigationController(rootViewController:PRAddSuccessViewController())
         window?.rootViewController = vc
         //vc.navigationBar.isHidden = true
         
