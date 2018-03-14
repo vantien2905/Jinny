9d11b3646e77a43ea0d62440746fa62cdb95c90b
Lamp <lamp@vinova.sg>
2018-03-14 09:09:39 +0700

    Resolve Linking error

---
+++ Prelens-jinny/Views/Controllers/Membership/ListMembership/PRMemberShipVC.swift
@@ -0,0 +1,26 @@
+//
+//  PRMemberShipVC.swift
+//  Prelens-jinny
+//
+//  Created by Lamp on 13/3/18.
+//  Copyright © 2018 Lamp. All rights reserved.
+//
+
+import UIKit
+
+class PRMemberShipVC: PRBaseNavigationViewController {
+    
+    @IBOutlet weak var cvMembership: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setTitle(title: "Jinny")
+    }
+
+
+
+}
+
+
+
---
+++ Prelens-jinny/Views/Controllers/Membership/ListMembership/PRMembershipCell.swift
@@ -0,0 +1,19 @@
+//
+//  PRMembershipCell.swift
+//  Prelens-jinny
+//
+//  Created by Lamp on 13/3/18.
+//  Copyright © 2018 Lamp. All rights reserved.
+//
+
+import UIKit
+
+class PRMembershipCell: UICollectionViewCell {
+    @IBOutlet weak var imvMemberPhoto: UIImageView!
+    @IBOutlet weak var imvStarred: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+      
+    }
+
+}
