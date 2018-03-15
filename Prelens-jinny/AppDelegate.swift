43d1b164e45b5af6e9631267742663753f0c57a2
Lamp <lamp@vinova.sg>
2018-03-15 15:00:21 +0700

    Create the tabbarbutton and tabbarcustom

--- Prelens-jinny/AppDelegate.swift
+++ Prelens-jinny/AppDelegate.swift
@@ -28,7 +28,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 //        enumerateFonts()
-        handleFlow()
+//        handleFlow()
+        let vc  = UINavigationController(rootViewController:PRNewBaseVC())
+        window?.rootViewController = vc
         return true
     }
     
---
+++ Prelens-jinny/Views/Controllers/Base/PRNewBaseVC.swift
@@ -0,0 +1,61 @@
+//
+//  PRNewBaseVC.swift
+//  Prelens-jinny
+//
+//  Created by Lamp on 15/3/18.
+//  Copyright © 2018 Lamp. All rights reserved.
+//
+
+import UIKit
+
+class PRNewBaseVC: UIViewController {
+
+    @IBOutlet weak var vDetail: UIView!
+    
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var vNavigation: UIView!
+    @IBOutlet weak var btnLeft: UIButton!
+    @IBOutlet weak var btnRight: UIButton!
+    
+
+    @IBOutlet weak var vTabbar: PRTabbarCustom!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpView()
+    }
+    
+    func setUpView() {
+        
+        lbTitle.text = "JINNY"
+        self.navigationController?.navigationBar.isHidden = true
+        vNavigation.backgroundColor = PRColor.mainAppColor
+        btnLeft.isHidden = true
+    }
+    
+    func setTitle(title: String) {
+        lbTitle.text = title
+    }
+    
+    func setBackButton(image: UIImage) {
+        btnLeft.imageView?.image = image
+    }
+    
+    func setRightButton(image: UIImage) {
+        btnRight.imageView?.image = image
+    }
+    
+    
+}
+//MARK: Button Action
+extension PRNewBaseVC {
+    @IBAction func btnLeftAction() {
+        self.pop()
+    }
+    
+    @IBAction func btnRightAction() {
+        
+    }
+}
+
+
