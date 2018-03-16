d2648653198f62e9f3caa27810e7aff161c189bc
Edward <edward@vinova.sg>
2018-03-16 16:24:34 +0700

    Add fabric

--- Prelens-jinny/AppDelegate.swift
+++ Prelens-jinny/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -27,6 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        Fabric.with([Crashlytics.self])
         handleFlow()
         return true
     }
