afec33338d1bf2e97dc977fb6bb99102a1a1c6d5
Rio <rio@vinova.sg>
2018-03-19 08:21:38 +0700

    #Design Setting UI

--- Prelens-jinny/Views/Controllers/Setting/SettingViewController.swift
+++ Prelens-jinny/Views/Controllers/Setting/SettingViewController.swift
@@ -1,24 +1,27 @@
 //
-//  PRSettingViewController.swift
+//  SettingViewController.swift
 //  Prelens-jinny
 //
-//  Created by vinova on 3/8/18.
+//  Created by vinova on 3/17/18.
 //  Copyright © 2018 Lamp. All rights reserved.
 //
 
 import UIKit
 
-class PRSettingViewController: UIViewController {
+class SettingViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func btnChangePasswordTapped(_ sender: Any) {
+        let changePasswordVC = PRChangePassWordViewController.initControllerFromNib()
+            self.push(controller: changePasswordVC)
     }
     
 
