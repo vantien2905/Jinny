9bcf4464fbcebb82b7f3d611bc715f9e325988f5
Felix Dinh <felix@vinova.sg>
2018-03-26 12:03:53 +0700

    Fix ui for membership detail

--- Prelens-jinny/Views/Controllers/Promotion/PromotionDetail/HeaderCell/PromotionDetailHeaderCell.swift
+++ Prelens-jinny/Views/Controllers/Promotion/PromotionDetail/HeaderCell/PromotionDetailHeaderCell.swift
@@ -10,32 +10,32 @@ import UIKit
 
 class PromotionDetailHeaderCell: UICollectionViewCell {
 
-    @IBOutlet weak var tvName: UITextView!
+    @IBOutlet weak var lbName: UILabel!
     @IBOutlet weak var lbExpireDate: UILabel!
     
-    @IBOutlet weak var lcsMerchantNameHeight: NSLayoutConstraint!
-    @IBOutlet weak var lcsSideConstraintName: NSLayoutConstraint!
-    
+//    @IBOutlet weak var lcsMerchantNameHeight: NSLayoutConstraint!
+//    @IBOutlet weak var lcsSideConstraintName: NSLayoutConstraint!
+//
     override func awakeFromNib() {
         super.awakeFromNib()
     }
 
     func setUpView(with data: Promotion) {
         self.backgroundColor = .white
-        tvName.isUserInteractionEnabled = false
-        tvName.backgroundColor = .clear
+        lbName.isUserInteractionEnabled = false
+        lbName.backgroundColor = .clear
         
-        lcsMerchantNameHeight.constant = getHeight(with: data)
+//        lcsMerchantNameHeight.constant = getHeight(with: data)
 
-        tvName.text = data.merchant?.name
+        lbName.text = data.merchant?.name
         guard let expiryDate = data.expiresAt else { return }
         lbExpireDate.text = "Expiry date: " + expiryDate
     }
     
-    func getHeight(with merchant: Promotion) -> CGFloat {
-        guard let text = merchant.merchant?.name else { return 0 }
-        let height = text.height(withConstrainedWidth: UIScreen.main.bounds.width - 2*lcsSideConstraintName.constant,
-                                 font: UIFont(name: "SegoeUI-Semibold", size: 17)!)
-        return height
-    }
+//    func getHeight(with merchant: Promotion) -> CGFloat {
+//        guard let text = merchant.merchant?.name else { return 0 }
+////        let height = text.height(withConstrainedWidth: UIScreen.main.bounds.width - 2*lcsSideConstraintName.constant,
+////                                 font: UIFont(name: "SegoeUI-Semibold", size: 17)!)
+//        return height
+//    }
 }
