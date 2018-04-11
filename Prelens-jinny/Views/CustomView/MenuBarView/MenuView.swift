a50512d5a227a5e7eb6f8d8d0807d1a1a407e9df
Felix Dinh <felix@vinova.sg>
2018-04-11 16:16:15 +0700

    Fix navigation bar hide/show

--- Prelens-jinny/Views/CustomView/MenuBarView/MenuView.swift
+++ Prelens-jinny/Views/CustomView/MenuBarView/MenuView.swift
@@ -86,7 +86,7 @@ class MenuView: UIView {
         //get cell selected
         let indexPath = IndexPath(item: index, section: 0)
         guard let cellScroll = collectionView.cellForItem(at: indexPath) as? MenuItemCollectionViewCell else { return }
-
+        self.indexSelected = index
         UIView.animate(withDuration: 0.3) {
             //-- scroll view horizontal
             self.vScrollBar.frame = CGRect(x: self.vScrollBar.frame.minX, y: self.vScrollBar.frame.minY, width: cellScroll.frame.width, height: self.vScrollBar.frame.height)
